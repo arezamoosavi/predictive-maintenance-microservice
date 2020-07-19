48eefc5ab9bd82290de726413ab84f4c4b6d684c
alimo <sdamoosavi@gmail.com>
2020-07-19 08:58:56 +0430

    machine learning micreservice works!

---
+++ microservices/fastapi/apis.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel, conlist
+from nameko.standalone.rpc import ServiceRpcProxy
+import os
+import json
+
+
+def rpc_proxy():
+    # the ServiceRpcProxy instance isn't thread safe so we constuct one for
+    # each request; a more intelligent solution would be a thread-local or
+    # pool of shared proxies
+    config = {"AMQP_URI": os.getenv("RMQ")}
+    return ServiceRpcProxy("mltask", config)
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def health_check():
+    return {"Hello": "World"}
+
+
+class DataModel(BaseModel):
+    data: conlist(float, min_items=15, max_items=15)
+
+
+@app.post("/pred/")
+async def predict(sent_data: DataModel):
+    try:
+        with rpc_proxy() as task_proxy:
+            prediction = task_proxy.get_predict(sent_data.data)
+        return {"messege": "Successfully Done!", "response": json.loads(prediction)}
+    except Exception as e:
+        print("EXECPTION: ", e)
+        return {"messege": "Error!", "response": "Error!"}
---
+++ microservices/ml-service/services.py
@@ -0,0 +1,54 @@
+import uuid
+import json
+import eventlet
+from eventlet.event import Event
+from nameko.rpc import rpc
+from nameko.extensions import DependencyProvider
+
+from joblib import load
+
+clf = load("forest_model.joblib")
+
+
+def randomforest(data):
+    result = clf.predict([data])
+    eventlet.sleep()  # won't yield voluntarily since there's no i/o
+    return result
+
+
+class PredictLife(DependencyProvider):
+    def predict(self, data):
+        # generate unique id
+        task_id = uuid.uuid4().hex
+
+        # execute it in a container thread and send the result to an Event
+        event = Event()
+        gt = self.container.spawn_managed_thread(lambda: randomforest(data))
+        gt.link(lambda res: event.send(res.wait()))
+
+        while True:
+            if event.ready():
+                life_cycle = event.wait()
+                retJson = {
+                    "data": data,
+                    "life_cycle": life_cycle[0],
+                    "task_id": task_id,
+                }
+                return json.dumps(retJson)
+            eventlet.sleep()
+
+    def get_dependency(self, worker_ctx):
+        class MLApi(object):
+            predict = self.predict
+
+        return MLApi()
+
+
+class MLService(object):
+    name = "mltask"
+
+    processor = PredictLife()
+
+    @rpc
+    def get_predict(self, data):
+        return self.processor.predict(data)
