3492834099a79d7d587d714f85e75478cb40ae40
alireza <sdamoosavi@gmail.com>
2020-08-11 13:19:27 +0430

    hbase retriving all data added

--- microservices/db-service/services.py
+++ microservices/db-service/services.py
@@ -16,6 +16,22 @@ def get_hbase_table(table_name):
     return table
 
 
+def all_data_hbase(table_name):
+    table = get_hbase_table(table_name)
+    ret_file = []
+    for key, value in table.scan():
+        print(key, value)
+        jfile = {
+            "time": key.decode("utf-8"),
+            "data": value[b"s:data"].decode("utf-8"),
+            "life_cycle": value[b"s:life_cycle"].decode("utf-8"),
+            "task_id": value[b"s:task_id"].decode("utf-8"),
+        }
+        print(jfile)
+        ret_file.append(jfile)
+    return ret_file
+
+
 def save_to_hbase(data):
     table = get_hbase_table("nasa-data")
     now = datetime.now()
@@ -37,7 +53,7 @@ def save_to_hbase(data):
     return True
 
 
-class SaveHbase(DependencyProvider):
+class HbaseHndler(DependencyProvider):
     def save_to(self, data):
 
         event = Event()
@@ -50,9 +66,24 @@ class SaveHbase(DependencyProvider):
                 return is_saved
             eventlet.sleep()
 
+    def all_data(self, table_name):
+
+        event = Event()
+        gt = self.container.spawn_managed_thread(lambda: all_data_hbase(table_name))
+        gt.link(lambda res: event.send(res.wait()))
+
+        while True:
+            if event.ready():
+                retFile = event.wait()
+                retJson = {"results": retFile}
+
+                return json.dumps(retJson)
+            eventlet.sleep()
+
     def get_dependency(self, worker_ctx):
         class DBApi(object):
             save_to = self.save_to
+            all_data = self.all_data
 
         return DBApi()
 
@@ -60,8 +91,12 @@ class SaveHbase(DependencyProvider):
 class DBService(object):
     name = "dbtask"
 
-    processor = SaveHbase()
+    processor = HbaseHndler()
 
     @rpc
     def save_the_data_to_hbase(self, data):
         return self.processor.save_to(data)
+
+    @rpc
+    def get_all_data(self, table_name):
+        return self.processor.all_data(table_name)
--- microservices/fastapi/apis.py
+++ microservices/fastapi/apis.py
@@ -6,12 +6,12 @@ from pydantic import BaseModel, conlist
 from nameko.standalone.rpc import ServiceRpcProxy
 
 
-def rpc_proxy():
+def rpc_proxy(job):
     # the ServiceRpcProxy instance isn't thread safe so we constuct one for
     # each request; a more intelligent solution would be a thread-local or
     # pool of shared proxies
     config = {"AMQP_URI": os.getenv("RMQ")}
-    return ServiceRpcProxy("mltask", config)
+    return ServiceRpcProxy(job, config)
 
 
 app = FastAPI()
@@ -29,9 +29,20 @@ class DataModel(BaseModel):
 @app.post("/pred/")
 async def predict(sent_data: DataModel):
     try:
-        with rpc_proxy() as task_proxy:
+        with rpc_proxy("mltask") as task_proxy:
             prediction = task_proxy.get_predict(sent_data.data)
         return {"messege": "Successfully Done!", "response": json.loads(prediction)}
+    except Exception as e:
+        print("EXECPTION: ", e)
+        return {"messege": e, "response": "Error!"}
+
+
+@app.get("/dataset/")
+async def all_data():
+    try:
+        with rpc_proxy("dbtask") as task_proxy:
+            query_results = task_proxy.get_all_data("nasa-data")
+        return {"messege": "Successfully Done!", "response": json.loads(query_results)}
     except Exception as e:
         print("EXECPTION: ", e)
         return {"messege": e, "response": "Error!"}
